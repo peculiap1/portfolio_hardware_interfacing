7825e3a69d78a736d2985ff2018603cdff9bf056
peculiap1 <150041463+peculiap1@users.noreply.github.com>
2024-06-04 13:11:09 +0200

    Opdracht 1 gemaakt

---
+++ README.md
@@ -0,0 +1,17 @@
+## Opdracht 1 
+
+### Opdracht 
+- 2 LED'S aansluiten op poort 10 en 13
+- LED's knipperen om en om (2s/2s)
+
+### Benodigdheden
+- Arduino + Breadboard
+- 2 LED's
+- 2 weerstanden (22 &#937;)
+
+### Sketch
+![opdracht1](img/opdracht1.png)
+
+### Video
+
+![]()
---
+++ code/opdracht1_code.ino
@@ -0,0 +1,20 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(13, OUTPUT);
+  pinMode(10, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(13, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(13, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(10, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(10, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
